c4ee943a89508cb8c00ee43aa70259b1f1946ae2
Sadman26 <fareless.ranger@gmail.com>
2023-12-28 16:13:13 +0600

    ok

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-var i;
-for (i = 0; i < 20; i++) {
-    console.log("Hello World")
+let arr = [23, 12, 2, 4, 1]
+let n = arr.length;
+let s = 0;
+let i = 0x33;
+while (i < n) {
+    s = s + arr[i];
+    i++;
 }
+console.log(s);
